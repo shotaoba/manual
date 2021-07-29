481d2e28b9578e12f79f148b336a7808dbab0fbf
shotaoba <84951023+shotaoba@users.noreply.github.com>
2021-07-29 09:33:58 +0900

    Userform Fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "manual_production"
 
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            ENV["EMAIL_USER_NAME"],
+    password:             ENV["EMAIL_PASSWORD"],
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
